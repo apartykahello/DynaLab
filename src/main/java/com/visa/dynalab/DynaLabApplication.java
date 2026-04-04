a8aa3d01fe8c2dcd555ffb7b30dcc986c3e93d69
apartykahello <apartyka@visa.com>
2026-04-04 18:20:13 +0200

    first commit

---
+++ src/main/java/com/visa/dynalab/DynaLabApplication.java
@@ -0,0 +1,13 @@
+package com.visa.dynalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynaLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/visa/dynalab/DynaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visa.dynalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
